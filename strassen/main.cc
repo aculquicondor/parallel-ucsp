e567da420c398748327dd32bcc85568fc7e794ef
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-03-31 19:13:45 -0500

    sequential Strassen algorithm

---
+++ strassen/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "matrix.h"
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	Matrix<int> a(n), b(n);
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> a(i, j);
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> b(i, j);
+	Matrix<int> c = a * b;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			cout << c(i, j) << ' ';
+		cout << endl;
+	}
+	return 0;
+}
